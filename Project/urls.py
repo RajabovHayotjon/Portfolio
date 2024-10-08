bf6000726a20ecdc9d215311d25e99945339fe6c
Hayot Rajabov <rajabovhayotjon777@gmail.com>
2024-10-08 17:21:03 +0500

    SS

--- Project/urls.py
+++ Project/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('portfolio/', include('myportfolio.urls')),
-    path('contact/', include('contact.urls')),
+    path('', include('myportfolio.urls')),
+    path('', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
